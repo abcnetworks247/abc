113299948b0b3d148a23f992d7e1f4bf56c159ea
prince ajuzie <princeajuzie1@gmail.com>
2023-12-10 09:04:52 +0100

    expecting new update from @temple

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -9,11 +9,7 @@ export default function Home() {
      <div className="flex flex-row sm:gap-10">
 
 	<div className="flex w-full flex-col p-4">
-		<div className="w-fit">
-    <label htmlFor="sidebar-mobile-fixed" className="btn-primary btn sm:hidden bg-red-500 text-white">Open Sidebar</label>
-		</div>
-
-    <label htmlFor="sidebar-mobile-fixed"  className="btn-primary btn sm:hidden bg-red-500 text-white">hello world </label>
+	
 
 	</div>
 </div>
