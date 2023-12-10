9cc7edafaf7e1e24b61bfbd85b98780493fc092f
prince ajuzie <princeajuzie1@gmail.com>
2023-12-10 20:28:48 +0100

    add: update on the navbar

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
-
+import { FaRegHeart } from "react-icons/fa";
+import { TiShoppingCart } from "react-icons/ti";
 export default function Navbar() {
   return (
     <div>
@@ -10,7 +11,41 @@ export default function Navbar() {
         <div className="navbar-start">
           <a className="navbar-item">Ripple UI</a>
         </div>
-        <div className="navbar-end flex flex-row items-center">
+        {/*  Pages  */}
+        <div class="navbar-center hidden md:block ">
+          <a class="navbar-item">Home</a>
+          <a class="navbar-item">Store</a>
+          <a class="navbar-item">Packages</a>
+          <a class="navbar-item">About</a>
+          <a class="navbar-item">Contact</a>
+        </div>
+        <div className="navbar-end flex flex-row items-center ">
+          <div className="flex flex-row items-center gap-3 mr-4">
+            <a
+              href="#"
+              className="text-center items-center flex flex-col text-gray-700 hover:text-primary transition relative"
+            >
+              <div className="text-2xl">
+                <FaRegHeart />
+              </div>
+              <div className="text-xs leading-3">Wishlist</div>
+              <div className="absolute right-0 -top-1 w-5 h-5 rounded-full flex items-center bg-red-500 justify-center text-white text-xs">
+                8
+              </div>
+            </a>
+            <a
+              href="#"
+              className="text-center items-center flex flex-col text-gray-700 hover:text-primary transition relative"
+            >
+              <div className="text-3xl">
+                <TiShoppingCart />
+              </div>
+              <div className="text-xs leading-3">cart</div>
+              <div className="absolute right-0 left-4 -top-1 w-5 h-5 rounded-full flex items-center bg-red-500 justify-center text-white text-xs">
+                8
+              </div>
+            </a>
+          </div>
           <div className="avatar avatar-ring avatar-md">
             <label className="btn btn-ghost  cursor-pointer px-0" tabIndex="0">
               <img
@@ -20,7 +55,7 @@ export default function Navbar() {
             </label>
           </div>
           <div className="avatar avatar-ring avatar-md">
-            <div className="dropdown-container  flex text-center ">
+            <div className="dropdown-container   text-center ">
               <label
                 htmlFor="sidebar-mobile-fixed"
                 className="btn btn-ghost flex cursor-pointer px-0 sm:hidden md:hidden lg:hidden"
@@ -35,7 +70,7 @@ export default function Navbar() {
                     className="btn btn-ghost cursor-pointer px-0 hidden sm:block md:block lg:block justify-items-end"
                     tabIndex="0"
                   >
-                    <MdOutlineKeyboardArrowDown   className="text-2xl" />
+                    <MdOutlineKeyboardArrowDown className="text-2xl mt-8" />
                   </label>
                   <div className="dropdown-menu dropdown-menu-bottom-left bg-white">
                     <a className="dropdown-item text-sm -z-50">Profile</a>
