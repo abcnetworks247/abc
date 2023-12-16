a061c6c3b8a1c5cc7d66d40c701f35102740544c
prince ajuzie <princeajuzie1@gmail.com>
2023-12-16 21:54:56 +0100

    sidebar update with new navigation

--- client/src/components/sidebar/Sidebar.js
+++ client/src/components/sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { IoStorefrontOutline } from "react-icons/io5";
 import { UseUserContext } from "../../../contexts/UserContext";
 import Image from "next/image";
@@ -6,6 +6,9 @@ import { MdClose } from "react-icons/md";
 import Link from "next/link";
 import { FaRegUser } from "react-icons/fa6";
 import { CgLivePhoto } from "react-icons/cg";
+import { BiHomeSmile } from "react-icons/bi";
+import { IoInformationCircleOutline } from "react-icons/io5";
+import { MdOutlineContacts } from "react-icons/md";
 
 export default function Sidebar() {
   const { loading, UserData, Authtoken } = UseUserContext();
@@ -22,76 +25,94 @@ export default function Sidebar() {
           className="sidebar-overlay lg:hidden "
         ></label>
         <aside className="justify-start h-full bg-white sidebar md:hidden lg:hidden sidebar-fixed-left sidebar-mobile max-sm:fixed max-sm:-translate-x-full">
-			<div className="flex items-center justify-between pr-6">
-
-          <section className="items-center p-4 sidebar-title">
-            <svg
-              fill="none"
-              height="42"
-              viewBox="0 0 32 32"
-              width="42"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <rect height="100%" rx="16" width="100%"></rect>
-              <path
-                clipRule="evenodd"
-                d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-                fill="currentColor"
-                fillRule="evenodd"
-              ></path>
-            </svg>
-            <div className="flex flex-col">
-              <span>Acme</span>
-              <span className="text-xs font-normal text-content2">
-                Team Plan
-              </span>
+          <div className="flex items-center justify-between pr-6">
+            <section className="items-center p-4 sidebar-title">
+              <svg
+                fill="none"
+                height="42"
+                viewBox="0 0 32 32"
+                width="42"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect height="100%" rx="16" width="100%"></rect>
+                <path
+                  clipRule="evenodd"
+                  d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
+                  fill="currentColor"
+                  fillRule="evenodd"
+                ></path>
+              </svg>
+              <div className="flex flex-col">
+                <span>Acme</span>
+                <span className="text-xs font-normal text-content2">
+                  Team Plan
+                </span>
+              </div>
+            </section>
+            <div>
+              <label htmlFor="sidebar-mobile-fixed">
+                <MdClose className="text-3xl cursor-pointer text-blackInverted" />
+              </label>
             </div>
-          </section>
-		  <div>
-			<label htmlFor="sidebar-mobile-fixed">
-
-		  <MdClose  className="text-3xl cursor-pointer text-blackInverted" />
-			</label>
-		  </div>
-			</div>
+          </div>
           <section className="sidebar-content">
             <nav className="rounded-md menu">
               <section className="px-4 menu-section">
                 <span className="menu-title">Main menu</span>
                 <ul className="menu-items">
-                  <li className="menu-item">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-5 h-5 opacity-75"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-                      />
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                      />
-                    </svg>
-                    <span>General</span>
-                  </li>
+                  <Link href="/">
+                    <li className="menu-item">
+                      <BiHomeSmile className="w-5 h-5 opacity-75" />
+                      <span>Home</span>
+                    </li>
+                  </Link>
 
                   <li className="menu-item">
                     <IoStorefrontOutline className="w-5 h-5 opacity-75" />
 
                     <a href="/store">Store</a>
                   </li>
+
                   <li className="menu-item">
                     <CgLivePhoto className="w-5 h-5 text-red-600 opacity-75" />
 
-                    <span className="px-4 py-1 text-sm font-medium text-white bg-red-600 rounded-lg">Live </span>
+                    <span className="px-4 py-1 text-sm font-medium text-white bg-red-600 rounded-lg">
+                      Live{" "}
+                    </span>
                   </li>
+
+                  <li className="menu-item">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="opacity-75"
+                      width="22"
+                      height="22"
+                      viewBox="0 0 24 24"
+                      strokeWidth="2"
+                      stroke="currentColor"
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                      <path d="M7 9m0 2a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2z"></path>
+                      <path d="M14 14m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+                      <path d="M17 9v-2a2 2 0 0 0 -2 -2h-10a2 2 0 0 0 -2 2v6a2 2 0 0 0 2 2h2"></path>
+                    </svg>
+                    Membership
+                  </li>
+
+                  <li className="menu-item">
+                  <IoInformationCircleOutline className="w-5 h-5 text-black opacity-75" />
+                    About
+                  </li>
+
+                  <li className="menu-item">
+                  <MdOutlineContacts className="w-5 h-5 text-black opacity-75" />
+                    Contact
+                  </li>
+
+          
                   <li>
                     <input
                       type="checkbox"
@@ -152,7 +173,7 @@ export default function Sidebar() {
               </section>
               <div className="my-0 divider"></div>
               <section className="px-4 menu-section">
-                <span className="menu-title">Settings</span>
+                <span className="menu-title">More</span>
                 <ul className="menu-items">
                   <li className="menu-item">
                     <svg
@@ -179,26 +200,7 @@ export default function Sidebar() {
                     </svg>
                     Payments
                   </li>
-                  <li className="menu-item">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="opacity-75"
-                      width="22"
-                      height="22"
-                      viewBox="0 0 24 24"
-                      strokeWidth="2"
-                      stroke="currentColor"
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                      <path d="M7 9m0 2a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2z"></path>
-                      <path d="M14 14m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
-                      <path d="M17 9v-2a2 2 0 0 0 -2 -2h-10a2 2 0 0 0 -2 2v6a2 2 0 0 0 2 2h2"></path>
-                    </svg>
-                    Balances
-                  </li>
+               
                   <li className="menu-item">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -279,10 +281,9 @@ export default function Sidebar() {
                     )}
                   </>
                 ) : (
-					<Link href="/login">
-					
-					<FaRegUser className="text-gray-700 hover:text-primary transition  text-[26px] cursor-pointer block md:hidden" />
-					</Link>
+                  <Link href="/login">
+                    <FaRegUser className="text-gray-700 hover:text-primary transition  text-[26px] cursor-pointer block md:hidden" />
+                  </Link>
                 )}
               </div>
             </div>
