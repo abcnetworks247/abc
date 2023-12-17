eea28bcee8f6f4e29a0b0e63b398b60741a42c2f
prince ajuzie <princeajuzie1@gmail.com>
2023-12-17 17:20:21 +0100

    fix

--- client/src/app/updatepassword/page.js
+++ client/src/app/updatepassword/page.js
@@ -1,60 +1,85 @@
-"use client"
+"use client";
 import { useState } from "react";
-import Link from "next/link"
+import Link from "next/link";
 import Api from "@/utils/Api";
+import { useSearchParams } from "next/navigation";
 export default function Page() {
-//  initial state for update password
+  // using usesearcparams to get the search params
+  const params = useSearchParams();
+
+  // using paams.get to get the reset query on the search params
+  const reset = params?.get("reset");
+  //  initial state for update password
   const [Allpassword, setAllpassword] = useState({
     password: "",
     confirmPassword: "",
-  })
+    reset,
+  });
 
   /**
-   * 
+   *
    * @param {object} e - the event object
    * @param {string} e.name - the name of the input
    * @param {string} e.value - the value of the input
    */
 
-  const HandleInputChange =(e) => { 
-    const{ name, value} = e.target
+  const HandleInputChange = (e) => {
+    const { name, value } = e.target;
     setAllpassword({
       ...Allpassword,
-      [name]: value
-    })
-  }
+      [name]: value,
+    });
+  };
 
-
-
-  const HandleSubmit = async (e)=> { 
-    e.preventDefault()
+  const HandleSubmit = async (e) => {
+    e.preventDefault();
     console.log(Allpassword);
     try {
-      await Api.post('client/auth/updatepassword', Allpassword)
+      const data = await Api.post("client/auth/updatepassword", Allpassword);
+      if (data.status === 201) {
+        console.log("post successful", data.data.message);
+        setTimeout(() => {
+          toast.dismiss(id);
+        }, 1000);
+        toast.update(id, {
+          render: `${data.data.message}`,
+          type: "success",
+          isLoading: false,
+        });
+        setTimeout(() => {
+          router.push("/login");
+        }, 3000);
+      }
     } catch (error) {
-      
+      const suberrormsg = toast.update(id, {
+        render: `${error}`,
+        type: "error",
+        isLoading: false,
+      });
+      setTimeout(() => {
+        toast.dismiss(suberrormsg);
+      }, 2000);
     }
-  }
+  };
 
   return (
-
-
-    <div >
+    <div>
       <div className="max-w-screen-xl h-screen sm:rounded-lg flex justify-center flex-1">
-        
         <div className="w-full  lg:w-1/2 xl:w-5/12 p-6  lg:flex-none flex items-center flex-col justify-center h-screen sm:p-12">
           <div className="w-[100%] flex flex-col items-center">
             <div className="text-center">
               <h1 className="text-2xl xl:text-4xl font-extrabold text-blue-900 mb-2">
-              Update Password
+                Update Password
               </h1>
               <p className="text-[12px] text-gray-500">
                 Enter your New Password
               </p>
             </div>
             <div className="w-full flex-1 mt-8">
-              <form className="mx-auto max-w-xs flex flex-col gap-4" onSubmit={HandleSubmit}>
-
+              <form
+                className="mx-auto max-w-xs flex flex-col gap-4"
+                onSubmit={HandleSubmit}
+              >
                 <input
                   className="w-full px-5 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
                   type="password"
@@ -69,15 +94,18 @@ export default function Page() {
                   placeholder="Confirm Password"
                   onChange={HandleInputChange}
                 />
-                
-                <button className="mt-5 tracking-wide font-semibold bg-blue-900 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none" onClick={HandleSubmit}>
+
+                <button
+                  className="mt-5 tracking-wide font-semibold bg-blue-900 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
+                  onClick={HandleSubmit}
+                >
                   <svg
                     className="w-6 h-6 -ml-2"
                     fill="none"
                     stroke="currentColor"
                     strokeWidth="2"
                     strokeLinecap="round"
-                   strokeLinejoin="round"
+                    strokeLinejoin="round"
                   >
                     <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
                     <circle cx="8.5" cy="7" r="4" />
@@ -105,7 +133,5 @@ export default function Page() {
         </div>
       </div>
     </div>
-
-
   );
 }
