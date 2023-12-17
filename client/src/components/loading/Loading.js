b96813dd01950728bfd343ff733cee9202fee11b
prince ajuzie <princeajuzie1@gmail.com>
2023-12-17 14:27:48 +0100

    new load added

--- client/src/components/loading/Loading.js
+++ client/src/components/loading/Loading.js
@@ -1,15 +1,16 @@
 
-import { RotatingLines } from  'react-loader-spinner'
+import { Blocks } from  'react-loader-spinner'
 
 export default function Loading() {
   return (
     <div className='h-[100vh] w-auto flex items-center justify-center m-auto'>
-      <RotatingLines
-  strokeColor="black"
-  strokeWidth="5"
-  animationDuration="0.75"
-  width="96"
+<Blocks
   visible={true}
+  height="80"
+  width="80"
+  ariaLabel="blocks-loading"
+  wrapperStyle={{}}
+  wrapperClass="blocks-wrapper"
 />
     </div>
   )
