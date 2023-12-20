8cfc230339c5501cf796cab40e231ff33968be96
Awajimijan Igoni <mijanigoni@gmail.com>
2023-12-20 17:03:28 +0100

    working on the edit functionality of the user profile

--- client/src/app/userdashboard/components/Editform.js
+++ client/src/app/userdashboard/components/Editform.js
@@ -1,18 +1,96 @@
+"use client"
 import React from 'react'
+import { useState } from 'react';
 
 const Editform = () => {
+
+   const [isEditable, setIsEditable] = useState(false);
+    const userData = {
+      firstName: "John",
+      lastName: "Doe",
+      email: "john.doe@example.com",
+      phoneNumber: "123-456-7890",
+      gender: "Male",
+    }
+
+    console.log(isEditable)
+
+    // Local state to store form data
+    const [formData, setFormData] = useState({
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+      email: userData.email,
+      phoneNumber: userData.phoneNumber,
+      gender: userData.gender,
+    })
+
+    // Function to handle form input changes
+    const handleInputChange = (e) => {
+      const { name, value } = e.target;
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }));
+    }
+
+    // Function to handle form submission
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      // Add logic to submit the form data (e.g., send it to the server)
+      console.log("Form data submitted:", formData)
+      // Set isEditable back to false after submission
+      setIsEditable(false)
+    };
+
+    const handleEdit = () => {
+        setIsEditable(true)
+    }
+
   return (
-      <form className="max-w-md mx-auto">
-          
+    <form className="max-w-md mx-auto" onSubmit={handleSubmit}>
+      <div className="flex flex-row-reverse cursor-pointer ">
+        <div
+          className="flex items-center justify-center w-8 h-8 rounded-full bg-gray-100 hover:bg-gray-200"
+          onClick={() => handleEdit()}
+        >
+          <svg
+            className="w-6 h-6"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            stroke="#737373"
+          >
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="m3.99 16.854-1.314 3.504a.75.75 0 0 0 .966.965l3.503-1.314a3 3 0 0 0 1.068-.687L18.36 9.175s-.354-1.061-1.414-2.122c-1.06-1.06-2.122-1.414-2.122-1.414L4.677 15.786a3 3 0 0 0-.687 1.068zm12.249-12.63 1.383-1.383c.248-.248.579-.406.925-.348.487.08 1.232.322 1.934 1.025.703.703.945 1.447 1.025 1.934.058.346-.1.677-.348.925L19.774 7.76s-.353-1.06-1.414-2.12c-1.06-1.062-2.121-1.415-2.121-1.415z"
+                fill="#737373"
+              ></path>
+            </g>
+          </svg>
+        </div>
+      </div>
+      {/* first and last name */}
       <div className="grid md:grid-cols-2 md:gap-6">
         <div className="relative z-0 w-full mb-5 group">
           <input
             type="text"
             name="floating_first_name"
             id="floating_first_name"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0  peer ${
+              isEditable ? " focus:border-blue-600" : ""
+            }`}
             placeholder=" "
-            required
+            value={formData.firstName}
+            onChange={handleInputChange}
+            readOnly={!isEditable}
           />
           <label
             for="floating_first_name"
@@ -26,9 +104,13 @@ const Editform = () => {
             type="text"
             name="floating_last_name"
             id="floating_last_name"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0  peer ${
+              isEditable ? "focus:border-blue-600" : ""
+            }`}
             placeholder=" "
-            required
+            value={formData.lastName}
+            onChange={handleInputChange}
+            readOnly={!isEditable}
           />
           <label
             for="floating_last_name"
@@ -37,16 +119,20 @@ const Editform = () => {
             Last name
           </label>
         </div>
-     </div>
-          
+      </div>
+      {/* Email address */}
       <div className="relative z-0 w-full mb-5 group">
         <input
           type="email"
           name="floating_email"
           id="floating_email"
-          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0  peer ${
+            isEditable ? " focus:border-blue-600" : ""
+          }`}
           placeholder=" "
-          required
+          value={formData.email}
+          onChange={handleInputChange}
+          readOnly={!isEditable}
         />
         <label
           for="floating_email"
@@ -56,81 +142,55 @@ const Editform = () => {
         </label>
       </div>
 
+      {/*phone number  */}
       <div className="relative z-0 w-full mb-5 group">
         <input
-          type="password"
-          name="floating_password"
-          id="floating_password"
-          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          type="text"
+          name="floating_email"
+          id="floating_email"
+          className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0  peer ${
+            isEditable ? " focus:border-blue-600" : ""
+          }`}
           placeholder=" "
-          required
+          value={formData.phoneNumber}
+          onChange={handleInputChange}
+          readOnly={!isEditable}
         />
         <label
-          for="floating_password"
-          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          for="floating_email"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
         >
-          Password
+          Phone Number
         </label>
       </div>
 
-      {/* <div className="relative z-0 w-full mb-5 group">
+      {/* gender */}
+      <div className="relative z-0 w-full mb-5 group">
         <input
-          type="password"
-          name="repeat_password"
-          id="floating_repeat_password"
-          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          type="text"
+          name="gender"
+          id="floating_email"
+          className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0  peer ${
+            isEditable ? " focus:border-blue-600" : ""
+          }`}
           placeholder=" "
-          required
+          value={formData.gender}
+          onChange={handleInputChange}
+          readOnly={!isEditable}
         />
         <label
-          for="floating_repeat_password"
-          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          for="floating_email"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
         >
-          Confirm password
+          Gender
         </label>
-      </div> */}
-
-      {/* <div className="grid md:grid-cols-2 md:gap-6">
-        <div className="relative z-0 w-full mb-5 group">
-          <input
-            type="tel"
-            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-            name="floating_phone"
-            id="floating_phone"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
-            required
-          />
-          <label
-            for="floating_phone"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            Phone number (123-456-7890)
-          </label>
-        </div>
-        <div className="relative z-0 w-full mb-5 group">
-          <input
-            type="text"
-            name="floating_company"
-            id="floating_company"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
-            required
-          />
-          <label
-            for="floating_company"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            Company (Ex. Google)
-          </label>
-        </div>
-      </div> */}
+      </div>
 
       <button
         type="submit"
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
-        Submit
+        {isEditable ? "Save" : "Submit"}
       </button>
     </form>
   );
