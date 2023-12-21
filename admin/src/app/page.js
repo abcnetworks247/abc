fe87afc0999806632b5d9f74a5250cecf68d5d32
prince ajuzie <princeajuzie1@gmail.com>
2023-12-21 07:28:35 +0100

    feat: side bar added

--- admin/src/app/page.js
+++ admin/src/app/page.js
@@ -1,9 +1,11 @@
 "use client"
+import Sidebar from "@/components/sidebar/Sidebar";
 import Image from "next/image";
 
 export default function Home() {
   return( 
   <main>
+    <Sidebar />
    <h1>dashboard here</h1>
   </main>
   
