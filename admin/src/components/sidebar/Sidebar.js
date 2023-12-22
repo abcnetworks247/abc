5356f1ddea2021228d0ce969e8f33eb0e170d3dd
templeajuzie <templeajuzie@gmail.com>
2023-12-22 16:18:23 +0100

    fix: side bar and upload

--- admin/src/components/sidebar/Sidebar.js
+++ admin/src/components/sidebar/Sidebar.js
@@ -14,6 +14,8 @@ import {
   Alert,
   Input,
 } from "@material-tailwind/react";
+import { FaRegFolderOpen } from "react-icons/fa6";
+
 import {
   PresentationChartBarIcon,
   ShoppingBagIcon,
@@ -131,18 +133,9 @@ export default function Sidebar() {
                 href="javascript:void(0)"
                 className="flex items-center px-4 py-3 text-sm text-white transition-all rounded hover:bg-gray-700"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  className="w-[18px] h-[18px] mr-4"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    d="M13 .5H3A2.503 2.503 0 0 0 .5 3v10A2.503 2.503 0 0 0 3 15.5h10a2.503 2.503 0 0 0 2.5-2.5V3A2.503 2.503 0 0 0 13 .5ZM14.5 13a1.502 1.502 0 0 1-1.5 1.5H3A1.502 1.502 0 0 1 1.5 13v-.793l3.5-3.5 1.647 1.647a.5.5 0 0 0 .706 0L10.5 7.207V8a.5.5 0 0 0 1 0V6a.502.502 0 0 0-.5-.5H9a.5.5 0 0 0 0 1h.793L7 9.293 5.354 7.647a.5.5 0 0 0-.707 0L1.5 10.793V3A1.502 1.502 0 0 1 3 1.5h10A1.502 1.502 0 0 1 14.5 3Z"
-                    data-original="#000000"
-                  />
-                </svg>
-                <span>Insight</span>
+                
+                <FaRegFolderOpen className="flex items-center px-4 py-3 text-sm text-white transition-all rounded hover:bg-gray-700" />
+                <span>File Manager</span>
               </a>
             </li>
             <li>
