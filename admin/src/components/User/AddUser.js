6f21a4401b2323ed006247e69c2720fb284c54fd
prince ajuzie <princeajuzie1@gmail.com>
2023-12-23 10:15:43 +0100

    add memeber working pefectly

--- admin/src/components/User/AddUser.js
+++ admin/src/components/User/AddUser.js
@@ -177,7 +177,7 @@ export function AddMember({ open, handleOpen }) {
                       </label>
                       <input
                         type="password"
-                        name="Password"
+                        name="password"
                         placeholder="******"
                         onChange={HandleInputChange}
                         className="focus:shadow-primary-outline dark:bg-slate-850 dark:placeholder:text-white/80 dark:text-white/80 text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none"
