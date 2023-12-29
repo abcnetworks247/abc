483a2dbdd88f311de84088c81d37665298adfafc
prince ajuzie <princeajuzie1@gmail.com>
2023-12-29 16:20:06 +0100

    fix: auth error message

---
+++ app/app/_layout.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const _layout = () => {
+  return (
+    <View>
+      <Text>_layout</Text>
+    </View>
+  )
+}
+
+export default _layout
---
+++ app/babel.config.js
@@ -0,0 +1,8 @@
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: ['expo-router/babel'],
+    plugins: ["nativewind/babel"]
+  };
+};
