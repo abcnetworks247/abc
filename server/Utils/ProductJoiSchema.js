b7e859d5186992b47f6e1bc9d6fa4853a28a77e7
templeajuzie <templeajuzie@gmail.com>
2023-12-30 16:58:18 +0100

    fix: route for creating product updated successfully

--- server/Utils/ProductJoiSchema.js
+++ server/Utils/ProductJoiSchema.js
@@ -1,20 +1,19 @@
 const Joi = require('joi');
 
 const ProductJoiSchema = Joi.object({
-  id: Joi.number().required(),
   title: Joi.string().required(),
   description: Joi.string().required(),
   price: Joi.number().required(),
-  discountPercentage: Joi.number().required(),
-  rating: Joi.number().required(),
+  discountPercentage: Joi.number(),
+  rating: Joi.number(),
   stock: Joi.number().required(),
-  brand: Joi.string().required(),
+  brand: Joi.string(),
   category: Joi.string().required(),
   thumbnail: Joi.string().required(),
   images: Joi.array().items(Joi.string()).required(),
-  color: Joi.string().required(),
-  warranty: Joi.string().required(),
-  weight: Joi.number().required(),
+  color: Joi.string(),
+  warranty: Joi.string(),
+  weight: Joi.number(),
 });
 
 module.exports = ProductJoiSchema;
