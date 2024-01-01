80e64adf4989033852f2e224f16158e6fd3f2cb3
templeajuzie <templeajuzie@gmail.com>
2024-01-01 18:37:17 +0100

    fix: edit news page for admin

--- admin/src/app/dashboard/news/edit/[id]/page.js
+++ admin/src/app/dashboard/news/edit/[id]/page.js
@@ -30,9 +30,7 @@ function page() {
   const { handleOpen, size } = UseFileManager();
   const { id } = useParams();
   // const [post, setPost] = useState([]);
-  const [imageSrc, setImageSrc] = useState(
-    "https://chatgen.ai/wp-content/uploads/2023/04/AI-chat-5-1200x675.png"
-  );
+  const [imageSrc, setImageSrc] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [full, setFull] = useState(true);
@@ -40,12 +38,12 @@ function page() {
   const [title, setTitle] = useState("");
   const [shortDescription, setShortDescription] = useState("");
   const [type, setType] = useState([]);
-  const [newCategory, setNewCategory] = useState("");
   const [newType, setNewType] = useState("");
+  const [newCategory, setNewCategory] = useState("");
   const [category, setCategory] = useState([]);
   const AuthToken = Cookies.get("adminToken");
   const router = useRouter();
-    
+
   const fetchData = async () => {
     try {
       const typeRes = await Api.get("admin/category/news/type");
@@ -77,6 +75,8 @@ function page() {
         setTitle(data.title);
         setShortDescription(data.shortdescription);
         setHtml(data.longdescription);
+        // setType(data.type);
+        // setNewCategory(data.category);
         setImageSrc(data.blogimage);
         setLoading(false);
       })
@@ -87,12 +87,11 @@ function page() {
       });
     fetchData();
   }, []);
-  
 
   const handleUpdate = async (e) => {
     try {
       e.preventDefault();
-  
+
       const data = {
         blogid: id,
         title: title,
@@ -102,22 +101,22 @@ function page() {
         type: newType,
         blogimage: imageSrc,
       };
-  
+
       setLoad(true);
       console.log("update data", data);
-  
+
       const res = await Api.patch(`admin/blog/update`, data, {
         headers: {
           Authorization: `Bearer ${String(AuthToken)}`,
         },
       });
-  
+
       console.log("update response", res);
-  
+
       if (res && res.status === 200 && res.data) {
         const message = res.data.message;
         setLoad(false);
-  
+
         Swal.fire({
           icon: "success",
           position: "center",
@@ -125,11 +124,11 @@ function page() {
           showConfirmButton: false,
           timer: 2000,
         });
-  
+
         router.push("/dashboard/news/all-news");
       } else {
         console.log("Unexpected response structure:", res);
-  
+
         setLoad(false);
         Swal.fire({
           icon: "error",
@@ -142,7 +141,7 @@ function page() {
       }
     } catch (error) {
       console.log(error);
-  
+
       setLoad(false);
       Swal.fire({
         icon: "error",
@@ -153,11 +152,15 @@ function page() {
       });
     }
   };
-  
 
   const handleChange = (e) => {
     setHtml(e.target.value);
   };
+
+  const HandleDeleteNewsimg = () => {
+    setImageSrc(null);
+  };
+
 
   return loading ? (
     //    spinner
@@ -185,7 +188,7 @@ function page() {
     </div>
   ) : (
     <div className="">
-      <div className=" h-full flex items-center justify-center px-4 py-12 bg-gray-100 sm:px-6 lg:px-8 m-0">
+      <div className="flex items-center justify-center h-full px-4 py-12 m-0 bg-gray-100 sm:px-6 lg:px-8">
         <div className="z-10 p-10 bg-white  lg:w-[70%] md:w-[80%] w-[90%] shadow-md rounded-xl">
           <div className="text-center">
             <h2 className="mt-5 text-3xl font-bold text-gray-900">
@@ -204,13 +207,19 @@ function page() {
               }
             </p>
           </div>
-          <form className="mt-8 space-y-3" onSubmit={(e) => {handleUpdate(e)}} method="POST">
+          <form
+            className="mt-8 space-y-3"
+            onSubmit={(e) => {
+              handleUpdate(e);
+            }}
+            method="POST"
+          >
             <div className="grid grid-cols-1 space-y-2">
               <label className="text-sm font-bold tracking-wide text-gray-500">
                 Title
               </label>
               <input
-                className="p-2 text-base border bg-gray-50 border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
+                className="p-2 text-base border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:border-indigo-500"
                 type=""
                 placeholder="Example: 10 Best Laptops"
                 onChange={(e) => setTitle(e.target.value)}
@@ -222,11 +231,12 @@ function page() {
               {/* a div with two option select for categories and type */}
               <div className="grid grid-cols-2 gap-2">
                 <select
-                  className="p-2 text-base border bg-gray-50 border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
+                  className="p-2 text-base border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:border-indigo-500"
                   name="type"
                   id="type"
                   required
-                  onChange={(e) => setNewType(e.target.value)}
+                  onChange={(e) => setType(e.target.value)}
+                  value={type}
                 >
                   <option hidden>Select Type</option>
                   {type &&
@@ -240,10 +250,11 @@ function page() {
                 </select>
 
                 <select
-                  className="p-2 text-base border bg-gray-50 border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
-                  name="type"
+                  className="p-2 text-base border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:border-indigo-500"
+                  name="n"
                   id="type"
                   required
+                  value={newCategory}
                   onChange={(e) => setNewCategory(e.target.value)}
                 >
                   <option hidden>Select Category</option>
@@ -270,7 +281,7 @@ function page() {
                 rows="10"
                 required
                 placeholder=" News details"
-                className="h-20 p-2 text-base border bg-gray-50 border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
+                className="h-20 p-2 text-base border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:border-indigo-500"
               ></textarea>
               <label className="text-sm font-bold tracking-wide text-gray-500">
                 Full Details
@@ -291,76 +302,97 @@ function page() {
                 </Editor>
               </EditorProvider>
             </div>
-            <div className="hidden grid-cols-1 space-y-2 ">
-              <label className="text-sm font-bold tracking-wide text-gray-500">
-                Attach Document
-              </label>
-            </div>
-            <div className="flex items-center justify-center w-full border rounded shadow-lg h-80">
-              
-                <div
-                  className="flex flex-col w-full h-full  border-2 border-gray-300 border-dashed rounded cursor-pointer hover:bg-gray-50"
-                  onClick={() => handleOpen("lg")}
-                >
-                  <div className="flex flex-col w-ful h-full items-center justify-center pt-5 pb-6">
+            <div className="flex flex-col gap-5">
+              <a href="#value=newsimage">
+                <div className="flex items-center justify-center w-full">
+                  <div
+                    className="flex flex-col w-full h-32 border-2 border-gray-300 border-dashed rounded cursor-pointer hover:bg-gray-50"
+                    onClick={() => handleOpen("lg")}
+                  >
+                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                      <svg
+                        className="w-10 h-10 text-gray-400"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
+                        />
+                      </svg>
+                      <p className="py-1 text-sm text-gray-600">
+                        Upload a file or drag and drop
+                      </p>
+                      <p className="text-xs text-gray-500">
+                        PNG, JPG, GIF up to 10MB
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </a>
+
+              {imageSrc && (
+                <div className="relative w-full">
+                  <Image
+                    className="w-full h-[50vh] object-cover rounded-lg"
+                    src={imageSrc}
+                    height={500}
+                    width={500}
+                    alt=""
+                  />
+                  <div
+                    className="absolute p-2 bg-gray-200 rounded-full cursor-pointer top-4 right-4"
+                    onClick={HandleDeleteNewsimg}
+                  >
                     <svg
-                      className="w-10 h-10 text-gray-400"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
+                      className="w-6 h-6 text-red-600 cursor-pointer"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
                       xmlns="http://www.w3.org/2000/svg"
                     >
                       <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
+                        fillRule="evenodd"
+                        d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                        clipRule="evenodd"
                       />
                     </svg>
-                    <p className="py-1 text-sm text-gray-600">
-                      Upload a file or drag and drop
-                    </p>
-                    <p className="text-xs text-gray-500">
-                      PNG, JPG, GIF up to 10MB
-                    </p>
                   </div>
                 </div>
-              
+              )}
             </div>
-            <p className="text-sm text-gray-300">
-              <span>File type: doc,pdf,types of images</span>
-            </p>
             <div>
               <button
                 type="submit"
                 className="flex justify-center w-full p-4 my-5 font-semibold tracking-wide text-gray-100 transition duration-300 ease-in bg-blue-500 rounded-full shadow-lg cursor-pointer focus:outline-none focus:shadow-outline hover:bg-blue-600"
               >
-                {
-                  load ? (
-                    <svg
-                      className="w-5 h-5 mr-3 -ml-1 text-white animate-spin"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                    >
-                      <circle
-                        className="opacity-25"
-                        cx="12"
-                        cy="12"
-                        r="10"
-                        stroke="currentColor"
-                        strokeWidth="4"
-                      />
-                      <path
-                        className="opacity-75"
-                        fill="currentColor"
-                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
-                      />
-                    </svg>
-                  ) : (
-                    <p>Upload</p>
-                  )
-                }
+                {load ? (
+                  <svg
+                    className="w-5 h-5 mr-3 -ml-1 text-white animate-spin"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    />
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+                    />
+                  </svg>
+                ) : (
+                  <p>Upload</p>
+                )}
               </button>
             </div>
           </form>
