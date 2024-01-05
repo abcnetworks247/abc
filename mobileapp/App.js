c159e4e753e684b235f40584c481302b4d958cdb
templeajuzie <templeajuzie@gmail.com>
2024-01-05 11:01:13 +0100

    fix: slogan

--- mobileapp/App.js
+++ mobileapp/App.js
@@ -4,7 +4,8 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View className="items-center justify-center flex-1 bg-white">
-      <Text>Klipto Inc!</Text>
+      <Text>Klipto Inc! </Text>
+      <Text>@2024 We no dey gree for anybody 😂😂😂</Text>
       <StatusBar style="auto" />
     </View>
   );
