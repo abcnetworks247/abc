ef862935cf6abc8daa723832ba0a3db63ac210f8
prince ajuzie <princeajuzie1@gmail.com>
2024-01-07 00:41:32 +0100

    feat: register page created for the app

--- mobileapp/app/_layout.js
+++ mobileapp/app/_layout.js
@@ -1,11 +1,11 @@
 
-import {Stack } from  "expo-router";
+// import {Stack } from  "expo-router";
 
-export default function RootLayout( ){
-   return<Stack>
-    <Stack.Screen name="index"  />
-    <Stack.Screen name="/auth/signup/index" options={{
-        headerTitle: "signup",
-    }} />
-   </Stack> 
-}
+// export default function RootLayout( ){
+//    return<Stack>
+//     {/* <Stack.Screen name="index"  />
+//     <Stack.Screen name="/auth/signup/index" options={{
+//         headerTitle: "signup",
+//     }} /> */}
+//    </Stack> 
+// }
