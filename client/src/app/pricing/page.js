d246400d0e2424eed96c651be7eb40b8e34697ae
silas-inegbe <kingssilas18@gmail.com>
2024-01-08 14:45:45 +0100

    Refactor pricing page layout

--- client/src/app/pricing/page.js
+++ client/src/app/pricing/page.js
@@ -198,7 +198,7 @@ export default function page() {
             <div className="lg:order-first">
               {
                 Plans.map((plan) => (
-                  <div className="flex flex-col">
+                  <div className="flex flex-row">
                   <div className="p-8 rounded-3xl bg-[#1c1f29] ring-1 ring-white/10 shadow-2xl">
                     <div className="flex justify-between">
                       <div className="flex items-center gap-3">
