858c576ead2a94574be9be3de853196c4c1190f5
silas-inegbe <kingssilas18@gmail.com>
2024-01-08 13:03:56 +0100

    Update width of container div in BlogComp.js

--- client/src/components/Blog/BlogComp.js
+++ client/src/components/Blog/BlogComp.js
@@ -87,7 +87,7 @@ export default function () {
           ))}
         </div>
       ) : (
-        <div className="w-full bg-red-200 flex justify-center items-center">
+        <div className="w-[100vw] bg-red-200 flex justify-center items-center">
           {/* <!-- component --> */}
           <div className="w-full  md:max-w-screen-xl md:px-10 border-2 ">
             <main className=" w-full">
