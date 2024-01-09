5831c97dbe9587a66e1e2bb5249eb3c25ca24bbd
Awajimijan Igoni <mijanigoni@gmail.com>
2024-01-09 17:29:14 +0100

    updating the user profile

--- client/src/app/userdashboard/components/update/Update.js
+++ client/src/app/userdashboard/components/update/Update.js
@@ -15,30 +15,22 @@ import { RotatingLines } from "react-loader-spinner";
 const Update = () => {
   const { UserData, HandleGetUser, Authtoken } = UseUserContext();
 
-  const [formData, setFormData] = useState(null);
+  const [formData, setFormData] = useState({
+    fullname: UserData?.fullname,
+    email: UserData.email,
+    phone: UserData.phone,
+    shippingaddress: UserData.shippingaddress,
+    userdp: UserData.userdp,
+  });
   const { screen } = UseProductProvider();
   const [selectedPhoto, setSelectedPhoto] = useState(null);
-  const [selecteddp, setSelectedDp] = useState(null);
   const [isEditable, setIsEditable] = useState(false);
   const [successMessage, setSuccessMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   console.log("UserData in form", formData);
 
-  useEffect(() => {
-    // Update formData when userData changes
-    setFormData({
-      fullname: UserData?.fullname,
-      email: UserData.email,
-      phone: UserData.phone,
-      shippingaddress: UserData.shippingaddress,
-      userdp: selecteddp,
-    });
-
-    setSelectedPhoto(UserData.userdp);
-  }, [UserData]);
-
-
+ 
   // Function to handle form input changes
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -55,9 +47,9 @@ const Update = () => {
     const imageUrl = URL.createObjectURL(selectedFile);
 
     setSelectedPhoto(imageUrl);
-    setSelectedDp(selectedFile);
-    console.log("selecteddp", selecteddp)
-    
+    setFormData({...formData, userdp:selectedFile})
+  
+    console.log("this is formData", formData)
   };
 
   const handleSubmit = async (e) => {
@@ -69,13 +61,12 @@ const Update = () => {
       const submitForm = new FormData();
 
       // Append form data to the FormData instance
-      // submitForm.append("fullname", formData?.fullname);
-      submitForm.append("fullname", formData?.fullname);
-      submitForm.append("email", formData?.email);
-      submitForm.append("userphoto", formData?.userdp);
-      submitForm.append("phone", formData?.phone);
-      submitForm.append("shippingaddress", formData?.shippingaddress);
-      submitForm.append("userdp", formData?.selecteddp);
+    
+      submitForm.append("fullname", formData.fullname);
+      submitForm.append("email", formData.email);
+      submitForm.append("phone", formData.phone);
+      submitForm.append("shippingaddress", formData.shippingaddress);
+      submitForm.append("userdp", formData.userdp);
 
       // Make a PATCH request
       const response = await axios.patch(
@@ -127,6 +118,7 @@ const Update = () => {
           handleSubmit={handleSubmit}
           handleImageChange={handleImageChange}
           selectedPhoto={selectedPhoto}
+          
         
         />
       ) : (
