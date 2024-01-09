6d94a67055067302150b4ab52ddf1c845fd7b42e
prince ajuzie <princeajuzie1@gmail.com>
2024-01-09 19:25:44 +0100

    feat: fix on drawer

--- mobileapp/app/(drawer)/_layout.js
+++ mobileapp/app/(drawer)/_layout.js
@@ -1,22 +1,29 @@
 import "react-native-gesture-handler"
-import { View, Text, SafeAreaView } from 'react-native'
-import {Drawer} from "expo-router/drawer"
+import { View, Text, SafeAreaView, Pressable } from 'react-native'
+import {Drawer} from "expo-router/drawer";
+import { DrawerContentScrollView, DrawerItem } from "@react-navigation/drawer";
 import React from 'react';
 import Navbar from '../components/navbar/Navbar';
 import {GestureHandlerRootView }from "react-native-gesture-handler";
 import globalStyels from "../../styles/globalStyels";
+import { useRouter } from "expo-router";
+
 
 const _layout = () => {
-
+  const Router = useRouter()
   return (
+    <>
+    
        <SafeAreaView style={globalStyels.droidSafeArea}>
 
      <Navbar />
-        <Drawer >
-        <Drawer.Screen name="pricing/index" options={{ header: () => null , animation: "slide_from_left"} } />
-        <Drawer.Screen name="(dashboard)" options={{ header: () => null , animation: "slide_from_left"} } />
+
+        <Drawer  >
+        <Drawer.Screen name="(dashboard)" options={{ header: () => null , title: "home",  animation: "slide_from_left"} } />
+      
     </Drawer>
         </SafeAreaView> 
+    </>
 
   )
 }
