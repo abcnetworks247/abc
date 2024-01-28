d6e27e3f19138fc9c23ad04defd87c133ed258df
templeajuzie <templeajuzie@gmail.com>
2024-01-29 00:48:25 +0100

    fix: bugs on the admin authentication

--- admin/src/app/auth/signin/page.js
+++ admin/src/app/auth/signin/page.js
@@ -15,6 +15,8 @@ function Page() {
   // for navigating to the dashboard page after login
   const router = useRouter();
   // initial state for form data
+
+  const [passwordVisible, setPasswordVisible] = useState(false);
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -154,6 +156,10 @@ function Page() {
     }
   };
 
+  const togglePasswordVisibility = () => {
+    setPasswordVisible((prevVisible) => !prevVisible);
+  };
+
   return (
     <>
       <ToastContainer
@@ -171,14 +177,14 @@ function Page() {
       <section className="flex gap-4 m-8">
         <div className="w-full mt-5 lg:w-3/5">
           <div className="text-center">
-          <Image 
-                src={Logo}
-                height={50}
-                width={50}
-                draggable={false}
-                className="object-contain h-[40px] w-full"
-                />
-            <Typography variant="h2" className="mb-4 font-bold">
+            <Image
+              src={Logo}
+              height={50}
+              width={50}
+              draggable={false}
+              className="object-contain h-[40px] w-full"
+            />
+            <Typography variant="h3" className="mt-4 mb-2 font-bold">
               Sign In
             </Typography>
             <Typography
@@ -186,7 +192,7 @@ function Page() {
               color="blue-gray"
               className="text-lg font-normal"
             >
-              Enter your email and password to Sign In.
+              Welcome back, sign in to continue. 👏
             </Typography>
           </div>
           <div className="max-w-screen-lg mx-auto mt-8 mb-2 w-80 lg:w-1/2">
@@ -210,22 +216,21 @@ function Page() {
                       }}
                       required
                       name="email"
-                      className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                      className="relative w-full min-w-[200px] flex items-center justify-between py-[22px] pl-3 pr-5 h-10 outline-none bg-white border border-gray-200 rounded-lg"
                     />
-                     {errorMessages.email && formData.email && (
+                    {errorMessages.email && formData.email && (
                       <span className="text-red-500 text-[13px]">
                         {errorMessages.email}
                       </span>
                     )}
-                    <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all before:content-none after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all after:content-none peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"></label>
+                    <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:transition-all before:content-none after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:transition-all after:content-none peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"></label>
                   </div>
                   <h6 className="block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900">
                     Password
                   </h6>
-                  <div className="relative h-11 w-full min-w-[200px]">
-                   
+                  <div className="relative h-11 w-full min-w-[200px] flex items-center justify-between py-2 pl-1 pr-5 h-10 bg-white border border-gray-200 rounded-lg">
                     <input
-                      type="password"
+                      type={passwordVisible ? "text" : "password"}
                       placeholder="********"
                       name="password"
                       onChange={(e) => {
@@ -238,36 +243,101 @@ function Page() {
                         );
                       }}
                       required
-                      className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                      className={`w-full h-full font-medium bg-transparent placeholder-gray-500 border-none text-sm outline-none border-0 border-transparent focus:outline-none focus:ring-0`}
                     />
-                  
-                    <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all before:content-none after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all after:content-none peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"></label>
+                    <div onClick={togglePasswordVisibility}>
+                      {passwordVisible ? (
+                        <svg
+                          className="w-5 h-5 cursor-pointer"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                          stroke="#737373"
+                        >
+                          <g id="SVGRepo_bgCarrier" strokeWidth={0} />
+                          <g
+                            id="SVGRepo_tracerCarrier"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                          <g id="SVGRepo_iconCarrier">
+                            {" "}
+                            <path
+                              d="M12 5C5.63636 5 2 12 2 12C2 12 5.63636 19 12 19C18.3636 19 22 12 22 12C22 12 18.3636 5 12 5Z"
+                              stroke="#737373"
+                              strokeWidth="1.5"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                            />{" "}
+                            <path
+                              d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z"
+                              stroke="#737373"
+                              strokeWidth="1.5"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                            />{" "}
+                          </g>
+                        </svg>
+                      ) : (
+                        <svg
+                          className="w-5 h-5 cursor-pointer"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                          stroke="#737373"
+                        >
+                          <g id="SVGRepo_bgCarrier" strokeWidth={0} />
+                          <g
+                            id="SVGRepo_tracerCarrier"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                          <g id="SVGRepo_iconCarrier">
+                            {" "}
+                            <path
+                              d="M20 14.8335C21.3082 13.3317 22 12 22 12C22 12 18.3636 5 12 5C11.6588 5 11.3254 5.02013 11 5.05822C10.6578 5.09828 10.3244 5.15822 10 5.23552M12 9C12.3506 9 12.6872 9.06015 13 9.17071C13.8524 9.47199 14.528 10.1476 14.8293 11C14.9398 11.3128 15 11.6494 15 12M3 3L21 21M12 15C11.6494 15 11.3128 14.9398 11 14.8293C10.1476 14.528 9.47198 13.8524 9.1707 13C9.11386 12.8392 9.07034 12.6721 9.04147 12.5M4.14701 9C3.83877 9.34451 3.56234 9.68241 3.31864 10C2.45286 11.1282 2 12 2 12C2 12 5.63636 19 12 19C12.3412 19 12.6746 18.9799 13 18.9418"
+                              stroke="#737373"
+                              strokeWidth="1.5"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                            />{" "}
+                          </g>
+                        </svg>
+                      )}
+                    </div>
+                    <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l  before:transition-all before:content-none after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r  after:transition-all after:content-none peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"></label>
                   </div>
                 </div>
                 {errorMessages.password && formData.password && (
-                      <span className="text-red-500 text-[13px]">
-                        {errorMessages.password}
-                      </span>
-                    )}
+                  <span className="text-red-500 text-[13px]">
+                    {errorMessages.password}
+                  </span>
+                )}
                 <button
                   className="mt-6 block w-full select-none rounded-lg bg-gray-900 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                   type="submit"
                   onClick={HandleSubmit}
                 >
-                  sign up
+                  Continue
                 </button>
               </form>
 
-              <Typography variant="small" className="font-medium text-gray-900">
-                <Link href="/auth/recover">Forgot Password?</Link>
+              <Typography
+                variant="small"
+                className="font-medium text-gray-900 flex flex-row gap-3 mt-3"
+              >
+                <span>Forgot Password?</span>
+                <Link href="/auth/recover" className="underline">
+                  Click Here
+                </Link>
               </Typography>
             </div>
           </div>
         </div>
         <div className="hidden w-2/5 h-full lg:block">
           <Image
-              height={200}
-              width={200}
+            height={200}
+            width={200}
             src="https://img.freepik.com/free-vector/realistic-news-studio-background_23-2149985600.jpg?w=900&t=st=1704041393~exp=1704041993~hmac=ef8c67168940ab32d52441d724c3e9071e9c512d39bb0c93b385396487e5aab3"
             className="object-cover w-full h-[90svh] rounded-3xl"
           />
