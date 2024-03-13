4ed34c40ed59d102785f192f8b45aa9c9a44c1b8
Temple Ajuzie <templeajuzie@gmail.com>
2024-03-13 12:02:24 +0100

    new data

--- server/routes/donationRoute.js
+++ server/routes/donationRoute.js
@@ -12,19 +12,25 @@ const router = require("express").Router();
 const authChecker = require("../middlewares/adminAuthChecker"); // Import your admin auth middleware
 const authChecker2 = require("../middlewares/clientAuthChecker"); // Import your admin auth middleware
 
+
 // Product Routes
 router.route("/products").get(getAllDonation); // Fetch all products
 router.route("/products/:id").get(getSingleDonation); // Fetch a single product
+
 
 router
   .route("/stripe/create-donate-session")
   .post(authChecker2, StripeCheckout); // Post a checkout session with Stripe
 
+
 router
   .route("/stripe/donate/webhook")
   .post(express.raw({ type: "application/json" }), stripeDonateWebhook); // Post a checkout session with Stripe
 
+  
+
 router.route("/crypto/create-checkout-session").post(authChecker2, Crypto); // Post a checkout session with Crypto
 router.route("/crypto/product/webhook").post(CryptoWebhook); // Post a checkout session with CryptoWebhook
+
 
 module.exports = router;
