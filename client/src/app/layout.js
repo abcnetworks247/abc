64a8e8e327c079c57597ac68f9d0bb0ebf0886ea
Temple Ajuzie <templeajuzie@gmail.com>
2024-03-14 17:02:04 +0100

    done

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,25 +1,23 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import ProductProvider from "../../contexts/ProductProvider";
-import { UserContextProvider } from "../../contexts/UserContext";
-const inter = Inter({ subsets: ["latin"] });
+import { Inter } from 'next/font/google';
+import './globals.css';
+import ProductProvider from '../../contexts/ProductProvider';
+import { UserContextProvider } from '../../contexts/UserContext';
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Abc studio",
-  description: "Informing the world",
+  title: 'Abc studio',
+  description: 'Informing the world',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta name="cryptomus" content="315c61b7" />
+        <meta name='cryptomus' content='315c61b7' />
       </head>
       <body className={inter.className}>
         <UserContextProvider>
-          <ProductProvider>
-            {children}
-          </ProductProvider>
+          <ProductProvider>{children}</ProductProvider>
         </UserContextProvider>
       </body>
     </html>
