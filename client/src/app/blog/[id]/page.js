c9b27ced4a052aeae6540bd13585125821ac91d7
Temple Ajuzie <templeajuzie@gmail.com>
2024-04-13 12:34:26 +0100

    feat: adding share feature

--- client/src/app/blog/[id]/page.js
+++ client/src/app/blog/[id]/page.js
@@ -23,6 +23,8 @@ export default function page() {
   const [shareModal, setShareModal] = useState(false);
   const [linkToShare, setLinkToShare] = useState(false);
 
+  const [commentSection, setCommentSection] = useState(false);
+
   //fetch blog api
   const fetchBlog = async () => {
     try {
@@ -88,10 +90,37 @@ export default function page() {
                     <address className='flex items-center mb-2 not-italic'>
                       {/* map through fetched data */}
                     </address>
-                    <h1 className='text-xl font-bold leading-tight text-gray-900 my-7 lg:mb-6 lg:text-2xl'>
+                    <h1 className='text-xl font-semibold leading-tight text-gray-900 my-7 lg:mb-6 lg:text-xl'>
                       {blog.title}
                     </h1>
                   </header>
+
+                  <div className='flex items-center justify-between gap-5 my-10'>
+                    <div className='flex items-center '>
+                      <img
+                        className='w-8 h-8 mr-2 rounded-full'
+                        src={author.userdp}
+                        alt='Profile picture of author'
+                      />
+                      <h3 className='text-base font-bold text-gray-900'>
+                        {author.fullname}:
+                      </h3>
+                      <p className='text-sm text-gray-800 ml-1'>
+                        Author
+                      </p>
+                    </div>
+                    <div className=''>
+                      <p className='text-sm text-gray-800 mx-4'>
+                        <time
+                          pubdate
+                          datetime='2022-02-08'
+                          title='February 8th, 2022'>
+                          13 April 2024
+                        </time>
+                      </p>
+                    </div>
+                  </div>
+
                   <figure>
                     <img
                       className='w-full h-auto mb-6 rounded-lg'
@@ -106,7 +135,7 @@ export default function page() {
                   <section className='mt-6 not-format'>
                     <div className='flex items-center justify-between mb-6'>
                       <h2 className='text-lg font-bold text-gray-900 lg:text-lg'>
-                        Discussion (20)
+                        Discussion (0)
                       </h2>
 
                       <h2
@@ -150,97 +179,17 @@ export default function page() {
                         Post comment
                       </button>
                     </form>
-                    <article className='p-6 mb-6 text-base bg-gray-200 rounded-lg '>
-                      <footer className='flex items-center justify-between mb-2'>
-                        <div className='flex items-center gap-5'>
-                          <div className='flex items-center '>
-                            <img
-                              className='w-8 h-8 mr-2 rounded-full'
-                              src={author.userdp}
-                              alt='Profile picture of author'
-                            />
-                            <h3 className='text-sm font-bold text-gray-900'>
-                              {author.fullname}
-                            </h3>
-                          </div>
-
-                          <p className='text-sm text-gray-800'>
-                            <time
-                              pubdate
-                              datetime='2022-02-08'
-                              title='February 8th, 2022'>
-                              Feb. 8, 2022
-                            </time>
-                          </p>
-                        </div>
-                        <button
-                          id='dropdownComment1Button'
-                          data-dropdown-toggle='dropdownComment1'
-                          className='inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:text-gray-400 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
-                          type='button'>
-                          <svg
-                            className='w-4 h-4'
-                            aria-hidden='true'
-                            xmlns='http://www.w3.org/2000/svg'
-                            fill='currentColor'
-                            viewBox='0 0 16 3'>
-                            <path d='M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z' />
-                          </svg>
-                          <span className='sr-only'>Comment settings</span>
-                        </button>
-                        {/* <!-- Dropdown menu --> */}
-                        <div
-                          id='dropdownComment1'
-                          className='z-10 hidden bg-white divide-y divide-gray-100 rounded shadow w-36 dark:bg-gray-700 dark:divide-gray-600'>
-                          <ul
-                            className='py-1 text-sm text-gray-700 dark:text-gray-200'
-                            aria-labelledby='dropdownMenuIconHorizontalButton'>
-                            <li>
-                              <a
-                                href='#'
-                                className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>
-                                Edit
-                              </a>
-                            </li>
-                            <li>
-                              <a
-                                href='#'
-                                className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>
-                                Remove
-                              </a>
-                            </li>
-                            <li>
-                              <a
-                                href='#'
-                                className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>
-                                Report
-                              </a>
-                            </li>
-                          </ul>
-                        </div>
-                      </footer>
-                      <p className='text-gray-600'>
-                        Very straight-to-point article. Really worth time
-                        reading. Thank you! But tools are just the instruments
-                        for the UX designers. The knowledge of the design tools
-                        are as important as the creation of the design strategy.
-                      </p>
-                      <div className='flex items-center mt-4 space-x-4'>
-                        <button
-                          type='button'
-                          className='flex items-center text-sm font-medium text-gray-500 hover:underline dark:text-gray-400'>
-                          <svg
-                            className='mr-1.5 w-3 h-3'
-                            aria-hidden='true'
-                            xmlns='http://www.w3.org/2000/svg'
-                            fill='currentColor'
-                            viewBox='0 0 20 18'>
-                            <path d='M18 0H2a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h2v4a1 1 0 0 0 1.707.707L10.414 13H18a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5 4h2a1 1 0 1 1 0 2h-2a1 1 0 1 1 0-2ZM5 4h5a1 1 0 1 1 0 2H5a1 1 0 0 1 0-2Zm2 5H5a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Zm9 0h-6a1 1 0 0 1 0-2h6a1 1 0 1 1 0 2Z' />
-                          </svg>
-                          Reply
-                        </button>
-                      </div>
-                    </article>
+                    {commentSection && (
+                      <article className='p-6 mb-6 text-base bg-gray-200 rounded-lg '>
+                        <p className='text-gray-600'>
+                          Very straight-to-point article. Really worth time
+                          reading. Thank you! But tools are just the instruments
+                          for the UX designers. The knowledge of the design
+                          tools are as important as the creation of the design
+                          strategy.
+                        </p>
+                      </article>
+                    )}
                   </section>
                 </article>
               </div>
